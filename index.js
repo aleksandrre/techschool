07cc47e7c6a0069f0773a006ee2b0ed1dcfc57e1
aleksandrre <alekochokheli.01@gmail.com>
2024-03-17 00:50:53 +0400

    rame2

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ import teacherRoute from "./routes/teacherRoute.js";
 import studentRoute from "./routes/studentRoute.js";
 import adminRoute from "./routes/adminRoute.js";
 import commonRoute from "./routes/commonRoute.js";
+import bcrypt from "bcrypt";
 
 // import informationRoute from "./routes/informationRoute.js";
 import potentialRoute from "./routes/potentialStudentRoute.js";
+import { Admin } from "./models/adminModel.js";
 
 const PORT = 3001;
 const app = express();
@@ -28,9 +30,31 @@ app.use("/common", commonRoute);
 
 // Database connection
 mongoose
-  .connect("mongodb://localhost:27017/school", {})
+  .connect(
+    // "mongodb://localhost:27017/your_database_name",
+    "mongodb+srv://alekochokheli01:iiD4teZWX9XwBy9I@cluster0.ucsj7po.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {}
+  )
   .then(() => {
     console.log("Successfully connected to MongoDB");
+    // Create an admin instance
+    // const newAdmin = new Admin({
+    //   username: "chokha",
+    //   // Hash the password using bcrypt
+    //   password: bcrypt.hashSync("123", 10),
+    //   role: "admin",
+    // });
+
+    // // Save the admin instance to the database
+    // newAdmin
+    //   .save()
+    //   .then((admin) => {
+    //     console.log("Admin created successfully:", admin);
+    //     // You can perform additional actions here if needed
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error creating admin:", error);
+    //   });
     app.listen(PORT, () => {
       console.log(`App is listening on ${PORT} port`);
     });
