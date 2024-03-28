174487a6bf2b0aae01e9ba054f761733526ac6fd
aleksandrre <alekochokheli.01@gmail.com>
2024-03-29 01:51:52 +0400

    add res.json

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,7 +54,9 @@ export const login = async (req, res) => {
 
     // Send the token to the client
     //დავაკომენტაროთ სექური და ონლი
-    res.cookie("accessToken", accessToken);
+    // res.cookie("accessToken", accessToken);
+    res.json({ token: accessToken, username: username, id: user.id });
+
     res.status(200).json({
       message: "Login successful",
       user: { username: user.username, role: user.role },
