0be8ab8f2529c1cd08574ffe334af420afa21e45
aleksandrre <alekochokheli.01@gmail.com>
2024-03-29 14:08:17 +0400

    remain previous version

--- index.js
+++ index.js
@@ -15,20 +15,12 @@ import { Admin } from "./models/adminModel.js";
 const PORT = 3001;
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
