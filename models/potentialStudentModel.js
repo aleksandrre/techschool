e2dd4172c6b8b946aa3aee00c3efff8e0f27eb36
aleksandrre <alekochokheli.01@gmail.com>
2024-04-05 22:10:45 +0400

    add promoCode field in potentialStudentSchema

--- models/potentialStudentModel.js
+++ models/potentialStudentModel.js
@@ -14,6 +14,10 @@ const potentialStudentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  promoCode: {
+    type: String,
+    required: true,
+  },
 });
 
 export const PotentialStudent = mongoose.model(
